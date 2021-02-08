ac3a5e7da0934c8ac43d5c343d9e010e81f62875
Komal Sharma <sh.komal1994@gmail.com>
2021-02-08 00:00:51 +0000

    nextGreaterElement in a stack

---
+++ src/OOPs/nextGreaterElement.java
@@ -0,0 +1,27 @@
+// Here, we are finding the next greater element for each element present inside the array
+
+package OOPs;
+
+import java.util.Stack;
+
+public class nextGreaterElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,1,3,8,6,7,5};
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<arr.length; i++){
+            while(!stack.isEmpty()&&arr[i]>stack.peek()){
+                int rv = stack.pop();
+                System.out.println(rv +" --> " +arr[i]);
+            }
+            stack.push(arr[i]);
+        }
+
+        while(!stack.isEmpty()){
+            int rv = stack.pop();
+            System.out.println(rv +" --> " +-1);
+        }
+    }
+}
--- src/OOPs/reverseAStack.java
+++ src/OOPs/reverseAStack.java
@@ -3,11 +3,11 @@ package OOPs;
 public class reverseAStack {
     public static void main(String[] args) throws Exception {
 
-        StackUsingArrays s = new StackUsingArrays(10);
+        StackUsingArrays s = new StackUsingArrays(5);
 
         // fill the stack
         System.out.println(s.size()); // 0
-        for (int i=0; i < 10; i++)
+        for (int i=0; i < 5; i++)
             s.push((i+1)*10);
         s.display();
 
@@ -16,8 +16,18 @@ public class reverseAStack {
         s.display();
     }
 
-    public static void reverseStack(StackUsingArrays stack, StackUsingArrays emptyStack, int index) throws Exception{
+    public static void reverseStack(StackUsingArrays stack, StackUsingArrays helper, int index) throws Exception{
+        if (stack.isEmpty())
+            return;
 
+        int item = stack.pop(); // 50, 40, 30, 20, 10 end
+        reverseStack(stack, helper, index+1);
+        helper.push(item);
 
+        if (index == 0) {
+            while (!helper.isEmpty()) {
+                stack.push(helper.pop());
+            }
+        }
     }
 }
