278e4aa8e593ae11540285edd78f2af36b85fe64
Komal Sharma <sh.komal1994@gmail.com>
2021-02-10 23:34:50 +0000

    Spiral Print and Insertion Sort

--- src/Arrays/insertionSort.java
+++ src/Arrays/insertionSort.java
@@ -1,7 +1,25 @@
 //Works on the logic when we have already sorted array and we want to merge the new element.
+package Arrays;
 
 public class insertionSort {
     public static void main(String[] args){
+        int[] arr = {30, 50, 10, 80, 40, 20};
+        insertionSrt(arr);
+        for(int val: arr)
+            System.out.print(val+" ");
+    }
 
+    /* Select counter from 1 and once it is selected keep going to the left (j)
+    until you satisfy condition. */
+    public static void insertionSrt(int[] arr){
+        for(int counter = 1; counter<arr.length; counter++){
+            int val = arr[counter];
+            int j = counter-1;
+            while(j>=0 && arr[j]>val){
+                arr[j+1] = arr[j];
+                j-=1;
+            }
+            arr[j+1] = val;
+        }
     }
 }
--- src/Arrays/spiralPrint.java
+++ src/Arrays/spiralPrint.java
@@ -4,27 +4,73 @@ import java.util.Scanner;
 
 public class spiralPrint {
     public static void main(String[] args){
-        int[][] arr = takeInput();
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+        System.out.print("Enter number of cols: ");
+        int columns = input.nextInt();
+        int[][] arr = new int[rows][columns];
+        arr = takeInput(arr, rows, columns);
+        printSpiral(arr, rows, columns);
+        input.close();
     }
 
-    public static int[][] takeInput(){
+    public static int[][] takeInput(int[][] arr, int rows, int columns){
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter number of rows: ");
-        int rows = input.nextInt();
-        System.out.println("Enter number of cols: ");
-        int columns = input.nextInt();
-
-        int[][] arr = new int[rows][columns];
-
-        System.out.println("Enter the values: ");
-
         for(int i=0; i<rows; i++)
             for (int j = 0; j< columns; j++){
+                System.out.print("Enter the values for arr["+i+"]["+j+"]: ");
                 arr[i][j] = input.nextInt();
             }
-
+        input.close();
         return arr;
+    }
+
+    public static void printSpiral(int[][] arr, int rows, int columns) {
+        int dir = 1;
+        int top = 0;
+        int bottom = rows-1;
+        int left = 0;
+        int right = columns-1;
+        int count = rows*columns;
+
+        while(left<=right && top<=bottom){
+            if (count>0){
+                if (dir == 1){
+                    for (int i=0; i<=right; i++){
+                        System.out.print(arr[top][i]+ " ");
+                        count-=1;
+                    }
+                    top+=1;
+                    dir+=1;
+                }
+
+                else if (dir==2){
+                    for (int i = top; i<=bottom; i++){
+                        System.out.print(arr[i][right]+ " ");
+                        count-=1;
+                    }
+                    right-=1;
+                    dir+=1;
+                }
+
+                else if (dir ==3){
+                    for (int i= right; i>=left; i--){
+                        System.out.print(arr[bottom][i]+" ");
+                        count-=1;
+                    }
+                    bottom-=1;
+                    dir+=1;
+                }
+
+                else {
+                    for(int i = bottom; i>top; i--){
+                        System.out.print(arr[i][left]+ " ");
+                    }
+                    left-=1;
+                    dir = 1;
+                }
+            }
+        }
     }
 }
